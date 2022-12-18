c9b908b4931e61ea22d9076ebae2b0884754ca39
LEMON <99194228+LIMON-AWA@users.noreply.github.com>
2022-12-18 15:11:14 -0500

    delete

--- src/main/java/com/dan/app/modelDAO/IProveedor.java
+++
@@ -1,9 +0,0 @@
-package com.dan.app.modelDAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dan.app.model.Proveedor;
-
-public interface IProveedor extends CrudRepository <Proveedor, Integer>{
-
-}
--- src/main/java/com/dan/app/modelDAO/IRegistrarTarjeta.java
+++
@@ -1,9 +0,0 @@
-package com.dan.app.modelDAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dan.app.model.Registrartarjeta;
-
-public interface IRegistrarTarjeta extends CrudRepository <Registrartarjeta, Integer>{
-
-}
