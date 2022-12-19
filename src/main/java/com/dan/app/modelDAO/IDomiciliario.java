906522249d0fc076907bcf138d813a4c781dccba
LEMON <99194228+LIMON-AWA@users.noreply.github.com>
2022-12-19 02:44:54 -0500

    htf

--- src/main/java/com/dan/app/modelDAO/IDomiciliario.java
+++
@@ -1,9 +0,0 @@
-package com.dan.app.modelDAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dan.app.model.Domiciliario;
-
-public interface IDomiciliario extends CrudRepository <Domiciliario, Integer>{
-
-}
