3123903b2ac19d526effbddfe631691b67fc0b78
LEMON <99194228+LIMON-AWA@users.noreply.github.com>
2022-12-19 18:44:21 -0500

    delete

--- src/main/java/com/dan/app/modelDAO/ICompra.java
+++
@@ -1,9 +0,0 @@
-package com.dan.app.modelDAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dan.app.model.Compra;
-
-public interface ICompra extends CrudRepository <Compra, Integer>{
-
-}
--- src/main/java/com/dan/app/modelDAO/IDomicilio.java
+++
@@ -1,9 +0,0 @@
-package com.dan.app.modelDAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dan.app.model.Domicilio;
-
-public interface IDomicilio extends CrudRepository <Domicilio, Integer>{
-
-}
